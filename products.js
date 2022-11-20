db227b23a3c47131e12cdfe8356f81b7184078d1
Karan Bawa <karan.bawa@nagarro.com>
2022-11-20 10:55:18 +0530

    code push

--- products.js
+++ products.js
@@ -8,24 +8,336 @@ fetch("people.json")
   .catch(function (err) {
     console.log("error: " + err);
   });
+// appendData([
+//   {
+//     id: "1",
+//     productName: "ANTIQUE BEIGE",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/ANTIQUE-BEIGE.jpg",
+//     alt: "ANTIQUE BEIGE",
+//   },
+//   {
+//     id: "2",
+//     productName: "ARGUS NERO",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/ARGUS-NERO.jpg",
+//     alt: "ARGUS NERO",
+//   },
+//   {
+//     id: "3",
+//     productName: "ARMANI BEIGE",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/ARMANI-BEIGE.jpg",
+//     alt: "ARMANI BEIGE",
+//   },
+//   {
+//     id: "4",
+//     productName: "ARMANI GREY",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/ARMANI-GREY.jpg",
+//     alt: "ARMANI GREY",
+//   },
+//   {
+//     id: "5",
+//     productName: "ARMONIA GREY",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/ARMONIA-GREY.jpg",
+//     alt: "ARMONIA GREY",
+//   },
+//   {
+//     id: "6",
+//     productName: "BLACK LAURENT",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/black-laurent.jpg",
+//     alt: "BLACK LAURENT",
+//   },
+//   {
+//     id: "7",
+//     productName: "BOTTOCHINO",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/BOTTOCHINO.jpg",
+//     alt: "BOTTOCHINO",
+//   },
+//   {
+//     id: "8",
+//     productName: "BRECCIA",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/BRECCIA.jpg",
+//     alt: "BRECCIA",
+//   },
+//   {
+//     id: "9",
+//     productName: "BROWNY STONE",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/BROWNY-STONE-800x1600.jpg",
+//     alt: "BROWNY STONE",
+//   },
+//   {
+//     id: "10",
+//     productName: "CAPACHINO BEIGE",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/CAPACHINO-BEIGE-800x1600.jpg",
+//     alt: "CAPACHINO BEIGE",
+//   },
+//   {
+//     id: "11",
+//     productName: "CREMA MARFIL",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/Crema-Marfil.jpg",
+//     alt: "CREMA MARFIL",
+//   },
+//   {
+//     id: "12",
+//     productName: "DYNA FANTASTIC",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/DYNA-FANTASTIC-800x1600.jpg",
+//     alt: "DYNA FANTASTIC",
+//   },
+//   {
+//     id: "13",
+//     productName: "EN. ANTIQUE CREMA",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/EN.-ANTIQUE-CREMA.jpg",
+//     alt: "EN. ANTIQUE CREMA",
+//   },
+//   {
+//     id: "14",
+//     productName: "GRIGIO CARNICO",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/GRIGIO-CARNICO-(B.jpg",
+//     alt: "GRIGIO CARNICO",
+//   },
+//   {
+//     id: "15",
+//     productName: "GRIGIO WILLIAM",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/GRIGIO-WILLIAM.jpg",
+//     alt: "GRIGIO WILLIAM",
+//   },
+//   {
+//     id: "16",
+//     productName: "LATTE IVORY",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/LATTE-IVORY.jpg",
+//     alt: "LATTE IVORY",
+//   },
+//   {
+//     id: "17",
+//     productName: "LEVIN NATURAL",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/LEVIN-NATURAL.jpg",
+//     alt: "LEVIN NATURAL",
+//   },
+//   {
+//     id: "18",
+//     productName: "MARFIL NATURAL",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/MARFIL-NATURAL-800x1600.jpg",
+//     alt: "MARFIL NATURAL",
+//   },
+//   {
+//     id: "19",
+//     productName: "QUANTUM PULPIS GREY",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/QUANTUM-PULPIS-GREY-800x1600-Large.jpg",
+//     alt: "QUANTUM PULPIS GREY",
+//   },
+//   {
+//     id: "20",
+//     productName: "RIVER STONE",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/RIVER-STONE.jpg",
+//     alt: "RIVER STONE",
+//   },
+//   {
+//     id: "21",
+//     productName: "ROYAL MARFIL",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/ROYAL-MARFIL.jpg",
+//     alt: "ROYAL MARFIL",
+//   },
+//   {
+//     id: "22",
+//     productName: "ROYAL ONYX",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/ROYAL-ONYX.jpg",
+//     alt: "ROYAL ONYX",
+//   },
+//   {
+//     id: "23",
+//     productName: "SATVARIO BRILLIANT",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/SATVARIO-BRILLIANT.jpg",
+//     alt: "SATVARIO BRILLIANT",
+//   },
+//   {
+//     id: "24",
+//     productName: "TOKYO IVORY",
+//     productDimensions: "800 x 1600 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/800x1600/TOKYO-IVORY.jpg",
+//     alt: "TOKYO IVORY",
+//   },
+//   {
+//     id: "25",
+//     productName: "ATLANTIS GOLD A",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/ATLANTIS-GOLD-A.jpg",
+//     alt: "ATLANTIS GOLD A",
+//   },
+//   {
+//     id: "26",
+//     productName: "EMPIRE WHITE A",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/EMPIRE_WHITE_A.jpg",
+//     alt: "EMPIRE WHITE A",
+//   },
+//   {
+//     id: "27",
+//     productName: "EMPIRE WHITE C",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/EMPIRE_WHITE_C.jpg",
+//     alt: "EMPIRE WHITE C",
+//   },
+//   {
+//     id: "28",
+//     productName: "MARKINA OLSPE R1",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/MARKINA_OLSPE_R1.jpg",
+//     alt: "MARKINA OLSPE R1",
+//   },
+//   {
+//     id: "29",
+//     productName: "NORDIC CAFFE P3",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/NORDIC_CAFFE_P3.jpg",
+//     alt: "NORDIC CAFFE P3",
+//   },
+//   {
+//     id: "30",
+//     productName: "PANDA WHITE",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/PANDA_WHITE.jpg",
+//     alt: "PANDA WHITE",
+//   },
+//   {
+//     id: "31",
+//     productName: "PUPLIS IVORY",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/PUPLIS_IVORY.jpg",
+//     alt: "PUPLIS IVORY",
+//   },
+//   {
+//     id: "32",
+//     productName: "SAINT LAURENT",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/SAINT_LAURENT.jpg",
+//     alt: "SAINT LAURENT",
+//   },
+//   {
+//     id: "33",
+//     productName: "SATUARIO FANTASTICO F1",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/SATUARIO_FANTASTICO_F1.jpg",
+//     alt: "SATUARIO FANTASTICO F1",
+//   },
+//   {
+//     id: "34",
+//     productName: "STATUARIO BIANCO SOFT",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/STATUARIO_BIANCO_SOFT.jpg",
+//     alt: "STATUARIO BIANCO SOFT",
+//   },
+//   {
+//     id: "35",
+//     productName: "TUDOR WHITE A",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/TUDOR_WHITE_A.jpg",
+//     alt: "TUDOR WHITE A",
+//   },
+//   {
+//     id: "36",
+//     productName: "VALVARINO NERO A",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/VALVARINO_NERO_A.jpg",
+//     alt: "VALVARINO NERO A",
+//   },
+//   {
+//     id: "37",
+//     productName: "VALVARINO NERO B",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/VALVARINO_NERO_B.jpg",
+//     alt: "VALVARINO NERO B",
+//   },
+//   {
+//     id: "38",
+//     productName: "VASAABHE",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/VASAABHE_1200X2400.jpg",
+//     alt: "VASAABHE",
+//   },
+//   {
+//     id: "39",
+//     productName: "VERSACHE GOLD",
+//     productDimensions: "1200 x 2400 mm",
+//     productType: "High Glossy Tiles",
+//     src: "./productImages/1200X2400/Versace_Gold.jpg",
+//     alt: "VERSACHE GOLD",
+//   },
+// ]);
 function appendData(data) {
   var mainContainer = document.getElementById("myData");
   for (var i = 0; i < data.length; i++) {
-    var htmlText = ``;
-    htmlText +=
-      '<li class="post-422 product type-product status-publish has-post-thumbnail product_cat-tile first instock shipping-taxable purchasable product-type-simple">';
-    htmlText +=
-      '<a rel="canonical" class="woocommerce-LoopProduct-link woocommerce-loop-product__link"';
-    htmlText += '<div style="background-color: white; border: 1px solid #fff">';
-    htmlText += '<div class="image-inner">';
-    htmlText += `<img src="${data.src}" class="tile_image wp-post-image" alt="Concrete Abstracts AzulGlazed Vitrified Tiles"/>`;
-    htmlText += `</div>`;
-    htmlText += `<div class="nt-ribbon">NEW</div>`;
-    htmlText += `<div class="clearfix"></div></div></a>`;
-    htmlText += `<div class="clearfix"></div>`;
-    htmlText += `<h2 class="woocommerce-loop-product__title">Concrete Abstracts Azul</h2>`;
-    htmlText += `<div class="tile-list-text"><p>Glazed Vitrified Tiles,1200 x 2500 mm</p></div></li>`;
-    // mainContainer.appendChild(htmlText);
-    mainContainer.innerHTML = htmlText;
+    console.log("data", data, data[i].src);
+    var div = document.createElement("li");
+    div.className = `post-422 product type-product status-publish has-post-thumbnail product_cat-tile first instock shipping-taxable purchasable product-type-simple`;
+    div.innerHTML += `<a rel="canonical" class="woocommerce-LoopProduct-link woocommerce-loop-product__link"`;
+    div.innerHTML += `<div style="background-color: white; border: 1px solid #fff">`;
+    div.innerHTML += `<div class="image-inner">`;
+    div.innerHTML += `<img src="${data[i].src}" class="tile_image wp-post-image" alt="${data[i].alt}"/>`;
+    div.innerHTML += `</div>`;
+    // div.innerHTML += `<div class="nt-ribbon">NEW</div>`;
+    div.innerHTML += `<div class="clearfix"></div></div></a>`;
+    div.innerHTML += `<div class="clearfix"></div>`;
+    div.innerHTML += `<h2 class="woocommerce-loop-product__title">${data[i].productName}</h2>`;
+    div.innerHTML += `<div class="tile-list-text"><p>${data[i].productDimensions}, ${data[i].productType}</p></div></li>`;
+    mainContainer.appendChild(div);
   }
 }
